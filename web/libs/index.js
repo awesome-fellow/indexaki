a327604b54def3051cc50fac537325f01dc0f9ee
kapekost <kapekost@gmail.com>
2017-04-24 21:21:14 +0100

    update material design

--- web/libs/index.js
+++ web/libs/index.js
@@ -1,5 +1,7 @@
 'use strict'
 require("./main.css");
+require("../../node_modules/bootstrap-material-design/dist/css/bootstrap-material-design.min.css")
+global.jQuery = global.$ = require('jquery');
 require("bootstrap-material-design");
 
 $.material.init()
