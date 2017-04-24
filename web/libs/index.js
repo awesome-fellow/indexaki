e1f6e99295ba5a4bae21a06a8fd12d1a99e35a79
kapekost <kapekost@gmail.com>
2017-04-24 02:06:54 +0100

    fix typo

--- web/libs/index.js
+++ web/libs/index.js
@@ -3,17 +3,3 @@ require("./main.css");
 require("bootstrap-material-design");
 
 $.material.init()
-
-{
-	output: {
-		// export itself to a global var
-		libraryTarget: "var",
-			// name of the global var: "Foo"
-			library: "Foo"
-	},
-	externals: {
-		// require("jquery") is external and available
-		//  on the global var jQuery
-		"jquery": "jQuery"
-	}
-}
