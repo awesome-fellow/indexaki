6346ab3777d2af2be44bad58a13ea1dfab7f1225
kapekost <kapekost@gmail.com>
2017-06-03 23:21:18 +0100

    add gifs for titles

---
+++ test/test-gif.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const restify = require('restify');
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+
+let client = null;
+
+before(function() {
+  const port = process.env.PORT || 8080;
+  // init the test client
+  client = restify.createJsonClient({
+    url: 'http://localhost:' + port,
+    version: '*'
+  });
+});
+
+describe('service: post get gif', function() {
+
+  it('should get a gif url', function(done) {
+    client
+      .get('/images/dog',
+      function(err, req, res, data) {
+        if (err) {
+          throw new Error(err);
+        }
+        else {
+          let body = JSON.parse(res.body);
+          assert.equal(res.statusCode, 200);
+          // expect(body.).to.exist;
+          done();
+        }
+      });
+  });
+})
